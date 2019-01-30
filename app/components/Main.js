a0348eaddffbbaf5896b5e287ce823d2420f2e6c
Subeg7 <subeg7@gmail.com>
2019-01-30 22:54:21 +0545

    creating layouts

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, TextInput,TouchableOpacity } from 'react-native-gesture-handler';
 
 export default class Main extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Testing</Text>
+       <View style={styles.header}>
+         <Text style={styles.headerText}>-Noter-</Text>
+       </View>
+
+       {/* <ScrollView style={styles.scrollContainer}>
+
+       </ScrollView > */}
+
+       <View style={styles.footer}>
+         <TextInput style={styles.textInput} placeholder="note" placeholderTextColor="green">
+         </TextInput>
+       </View>
+
+       <TouchableOpacity>
+        <Text>+</Text>
+       </TouchableOpacity>
       </View>
     );
   }
@@ -18,4 +34,31 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  headerText:{
+    color: 'red',
+    fontSize:18,
+    padding:26,
+  },
+
+  header:{
+    color: '#fff',
+    backgroundColor: '#405ae8',
+    borderBottomWidth:10,
+    borderBottomColor:'#af509c',
+    justifyContent:'center',
+  },
+
+  scrollContainer:{
+    color: '#0ff',
+  },
+
+  footer:{
+    color: '#fff',
+  },
+
+  hetextInputader:{
+    color: '#fff',
+  }
+
+
 });
