6655b33dc4420d22000b2a55221b4fa4de0360d9
Subeg7 <subeg7@gmail.com>
2019-01-31 20:46:30 +0545

    note file added

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,9 +1,22 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { ScrollView, TextInput,TouchableOpacity } from 'react-native-gesture-handler';
+import Note from './Note';
 
 export default class Main extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      noteArray:[],
+      noteText:'',
+    }
+  }
   render() {
+    let notes = this.state.noteArray.map( (val,key)=>{
+      return <Note key={key} keyval={key} val={val}
+        deleteMethod={()=>this.deleteNote(key)}/>
+    });
     return (
       <View style={styles.container}>
        <View style={styles.header}>
@@ -15,15 +28,26 @@ export default class Main extends React.Component {
        </ScrollView > */}
 
        <View style={styles.footer}>
-         <TextInput style={styles.textInput} placeholder="note" placeholderTextColor="green">
+         <TextInput style={styles.textInput} 
+         onChangeText={(noteText)=>this.setState({noteText})}
+         value={this.state.noteText}
+         placeholder="note" 
+         placeholderTextColor="green">
          </TextInput>
        </View>
 
-       <TouchableOpacity style={styles.buttonText}>
+       <TouchableOpacity 
+       onPress={this.addNote.bind(this)}
+       style={styles.buttonText}>
         <Text style={styles.addButton}>+</Text>
        </TouchableOpacity>
       </View>
     );
+  }
+
+  addNote(){
+    
+    alert('test passed');
   }
 }
 
@@ -61,7 +85,7 @@ const styles = StyleSheet.create({
   },
   buttonText:{
     // position:'absolute',
-    // zIndex:11,
+    // zIndex:11, 
     // right:20,
     // bottom:50,
     // fontSize:60,
--- app/components/Note.js
+++ app/components/Note.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import Main from'./app/components/Main';
+import { StyleSheet, Text, View } from 'react-native';
+import { ScrollView, TextInput,TouchableOpacity } from 'react-native-gesture-handler';
+
 
 export default class Note extends React.Component {
   render() {
     return (
-      <Main/>
+     <View key={this.props.keyval} style={styles.notes}>
+       <Text style={styles.noteText}>{this.props.val.date}</Text>
+       <Text style={styles.noteText}>{this.props.val.note}</Text>
+
+       <TouchOpacity onPress={this.props.deleteMethod} style={styles.noteDelete}>
+         <Text style={this.styles.noteDeleteText}>Delete</Text>
+       </TouchOpacity>
+     </View>
     );
   }
 }
