a6eed94c6839d029089e93d0ce2d569f501ead13
Subeg7 <subeg7@gmail.com>
2019-02-13 22:02:24 +0545

    bug fixed in add reducer

--- app/container/AddToDo.js
+++ app/container/AddToDo.js
@@ -3,13 +3,20 @@ import { StyleSheet, Text, View } from 'react-native';
 import { ScrollView, TextInput,TouchableOpacity } from 'react-native-gesture-handler';
 import Footer from './Footer'
 import Todo from './Todo'
-// import AddTodo from '../containers/AddTodo'
-// import VisibleTodoList from '../containers/VisibleTodoList'
+
+
 
 export default class Main extends React.Component {
   render() {
     return (
       <View style={styles.container}>
+
+
+        <Text>Redux on fire </Text>
+        <Text>Redux on fire </Text>
+        
+        
+        <Text>Redux on fire </Text>
         <Text>Redux on fire </Text>
         
       </View>
@@ -20,10 +27,14 @@ export default class Main extends React.Component {
 
 const styles = StyleSheet.create({
   container:{
+
     width: '100%',
-     height: '100%',
-     justifyContent: 'center',
-     alignItems: 'center'
+     
+    height: '100%',
+    
+    justifyContent: 'center',
+    
+    alignItems: 'center' 
 
   }
 
