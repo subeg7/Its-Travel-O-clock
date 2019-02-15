5592334a3451ddda48ec24455326edd4ff743b10
Subeg7 <subeg7@gmail.com>
2019-02-15 13:18:15 +0545

    addPlace action added

--- App.js
+++ App.js
@@ -1,19 +1,59 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { createStore } from 'redux';
+// App.js
 
-import Main from'./app/components/Main';
-
-export default class App extends React.Component {
-  render() {
+import React, {Component} from 'react';
+import { StyleSheet, View, TextInput, Button } from 'react-native';
 
 
 
-    return (
-      // <Main/>
-      <Main/>
-      // redux boiler plate template
+export default class App extends Component {
+
+  state = {
+    placeName: '',
+    places: []
+ }
+
+placeSubmitHandler = () => {
+    console.log("Submitted");	
+}
+
+render() {
+   return (
+    <View style={ styles.container }>
+       <View style = { styles.inputContainer }>
+        <TextInput
+           placeholder = "Seach Places"
+           style = { styles.placeInput }
+        ></TextInput>
+        <Button title = 'Add' 
+            style = { styles.placeButton }
+            onPress = { this.placeSubmitHandler }
+        />
+        </View>
+    </View>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: 30,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    // height:'%'
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    width: '100%'
+  },
+  placeInput: {
+    width: '70%'
+  },
+  placeButton: {
+    width: '30%'
+  },
+  listContainer: {
+    width: '100%'
+  }
+});
