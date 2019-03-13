277c3c3f20a3eeb5ac99862edfb0c522e82e3ebd
subeg7 <subeg7@gmail.com>
2019-03-13 22:19:32 +0545

    Snall change in container

--- app/container/VisibleTodoList.js
+++ app/container/VisibleTodoList.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { ScrollView, TextInput,TouchableOpacity } from 'react-native-gesture-handler';
+
+
 import Footer from './Footer'
 import Todo from './Todo'
